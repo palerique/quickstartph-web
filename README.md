796d50d39c6c2f2d17aec2388a30c1fb20d59d4a
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2014-11-14 13:12:12 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Quick Start PH
+========
+
+Como gerenciar e fazer releases do seu projeto com o git-flow e o maven
+
+http://sitedo.ph/como-gerenciar-e-fazendo-releases-seu-projeto-com-o-git-flow-e-o-maven/
+
---
+++ src/main/java/ph/sitedo/quickstartph/App.java
@@ -0,0 +1,15 @@
+package ph.sitedo.quickstartph;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World! Now is " + new Date() );
+    }
+}
